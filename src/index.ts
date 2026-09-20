d92699b02d7ac0d2def874514b19e12aaa5c36b6
Aarush Karak <worldshaans@gmail.com>
2026-09-20 13:41:40 -0400

    Update tailwind-plugin: 2026-09-20 14:30 [skip ci]

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import plugin from "tailwindcss/plugin.js";
-// Build: 2026-09-20 14:00:24 | run 1789927224
+// Build: 2026-09-20 14:30:09 | run 1789929009
 
 interface PluginOptions {
   glowColor?: string;
